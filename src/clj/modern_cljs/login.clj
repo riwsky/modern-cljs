c34ad35ff16f876ef5f75290322a5c83c1e5fc82
agofilo <francesco.agozzino86@gmail.com>
2013-01-25 16:22:56 +0100

    Added valitors with metis

--- src/clj/modern_cljs/login.clj
+++ src/clj/modern_cljs/login.clj
@@ -1,4 +1,5 @@
-(ns modern-cljs.login)
+(ns modern-cljs.login
+  (:require [metis.core :refer [defvalidator]]))
 
 (def ^:dynamic *re-email*
   #"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
@@ -6,16 +7,26 @@
 (def ^:dynamic *re-password*
   #"^(?=.*\d).{4,8}$")
 
+(defvalidator email-validator
+  [:email :formatted {:pattern *re-email* :message "Incorrect email format"}])
+
+(defvalidator password-validator
+  [:password :formatted {:pattern *re-password* :message "Incorrect password format"}])
+
+(defvalidator email-password-validator
+  [:email :formatted {:pattern *re-email* :message "Incorrect email format"}]
+  [:password :formatted {:pattern *re-password* :message "Incorrect password format"}])
+
 (declare validate-email validate-password)
 
 (defn authenticate-user [email password]
   (if (or (empty? email) (empty? password))
     (str "Please complete the form")
-    (if (and (validate-email email)
-             (validate-password password))
+    (if (empty? (email-password-validator {:email email :password password}))
       (str email " and " password
            " passed the formal validation, but you still have to be authenticated"))))
 
+(comment
 (defn validate-email [email]
   (if  (re-matches *re-email* email)
     true))
@@ -23,3 +34,5 @@
 (defn validate-password [password]
   (if  (re-matches *re-password* password)
     true))
+
+)
