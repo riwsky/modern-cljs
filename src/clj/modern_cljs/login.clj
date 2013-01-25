ee56a72c24e752527731c50567c2d0edc4d18787
agofilo <francesco.agozzino86@gmail.com>
2013-01-25 18:37:02 +0100

    Validation using valip

--- src/clj/modern_cljs/login.clj
+++ src/clj/modern_cljs/login.clj
@@ -1,4 +1,6 @@
-(ns modern-cljs.login)
+(ns modern-cljs.login
+  (:require [valip.core :refer [validate ]]
+            [valip.predicates :refer [matches]]))
 
 (def ^:dynamic *re-email*
   #"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
@@ -6,20 +8,41 @@
 (def ^:dynamic *re-password*
   #"^(?=.*\d).{4,8}$")
 
-(declare validate-email validate-password)
+(defn email-validator
+  [email]
+  (let [user-email {:email email}]
+    (validate user-email
+                 [:email (matches *re-email*) "Invalid email format"])))
 
+(defn password-validator
+  [password]
+  (let [user-password {:password password}]
+    (validate user-password
+                 [:password (matches *re-password*) "Invalid password format"])))
+
+(defn email-password-validation
+  [email password]
+  (let [user {:email email :password password}]
+    (validate user
+                 [:email (matches *re-email*) "Invalid email format"]
+                 [:password (matches *re-password*) "Invalid password format"])))
+;; (declare validate-email validate-password)
 (defn authenticate-user [email password]
   (if (or (empty? email) (empty? password))
     (str "Please complete the form")
-    (if (and (validate-email email)
-             (validate-password password))
+    (if (nil? (email-password-validation email password))
       (str email " and " password
            " passed the formal validation, but you still have to be authenticated"))))
 
-(defn validate-email [email]
-  (if  (re-matches *re-email* email)
-    true))
+(comment
+  (defn validate-email [email]
+    (if  (re-matches *re-email* email)
+      true))
+  
+  (defn validate-password [password]
+    (if  (re-matches *re-password* password)
+      true))
+  )
 
-(defn validate-password [password]
-  (if  (re-matches *re-password* password)
-    true))
+
+
