be04bc0700f1493a95bafab0234ae44b0edfdd43
agofilo <francesco.agozzino86@gmail.com>
2013-01-25 16:49:51 +0100

    Added bouncer validators

--- src/clj/modern_cljs/login.clj
+++ src/clj/modern_cljs/login.clj
@@ -1,4 +1,6 @@
-(ns modern-cljs.login)
+(ns modern-cljs.login
+  (:require [bouncer.core :refer [validate]]
+            [bouncer.validators :refer [defvalidator]]))
 
 (def ^:dynamic *re-email*
   #"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
@@ -6,20 +8,38 @@
 (def ^:dynamic *re-password*
   #"^(?=.*\d).{4,8}$")
 
+(defvalidator email-validator
+  [email]
+  (validate {:email email} 
+              :email (v/matches *re-email* :message "Invalid email format")))
+
+(defvalidator password-validator
+  [password]
+  (validate {:password password} 
+              :password (v/matches *re-password* :message "Invalid password format")))
+
+(defvalidator email-password-validator
+  [email password]
+  (validate {:email email :password password} 
+              :email (v/matches *re-email* :message "Invalid email format")
+              :password (v/matches *re-password* :message "Invalid password format")))
+
 (declare validate-email validate-password)
 
 (defn authenticate-user [email password]
   (if (or (empty? email) (empty? password))
     (str "Please complete the form")
-    (if (and (validate-email email)
-             (validate-password password))
+    (if (nil? (first (email-password-validator email password)))
       (str email " and " password
            " passed the formal validation, but you still have to be authenticated"))))
 
-(defn validate-email [email]
-  (if  (re-matches *re-email* email)
-    true))
 
-(defn validate-password [password]
-  (if  (re-matches *re-password* password)
-    true))
+(comment
+  (defn validate-email [email]
+    (if  (re-matches *re-email* email)
+      true))
+
+  (defn validate-password [password]
+    (if  (re-matches *re-password* password)
+      true))
+)  
